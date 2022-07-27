3d1184da4eddc6203f32fde2dbaf40d6b19b38c3
Bakeza <dbakeza2002@gmail.com>
2022-07-27 16:48:34 +0300

    merge

--- js/form.js
+++ js/form.js
@@ -1,53 +1,25 @@
-const addButton = document.querySelector("#add_btn");
+const addButton=document.querySelector('#add_btn')
 
 let i = 0;
-var selectedProductId = sessionStorage.getItem("productID");
-if (selectedProductId !== "null") {
-  add_btn.textContent = "EDIT";
-  window.localStorage.getItem("ArrayOfProducts");
-  let selectedProduct = staticProducts.filter((e) => {
-    console.log(
-      "selectedProductId == e.id => ",
-      e.id,
-      Number(selectedProductId),
-      e.id === selectedProductId
-    );
-    return e.id == selectedProductId;
-  })[0];
-  names.setAttribute("placeholder", selectedProduct.name);
-  price.setAttribute("placeholder", selectedProduct.price);
-  img_url.setAttribute("placeholder", selectedProduct.img_url);
-  category.setAttribute("placeholder", selectedProduct.category);
-  description.setAttribute("placeholder", selectedProduct.description);
-}
-if (btnCancel != null) {
-  btnCancel.addEventListener("click", cancelOperation);
-
-  add_btn.addEventListener("click", (e) => {
+if(addButton)
+addButton.addEventListener('click', (e) => {
     console.log(names.value);
+    e.preventDefault()
     let products = {
-      id: staticProducts.length,
-      name: names.value,
-      description: description.value,
-      price: price.value,
-      img_url: img_url.value,
-      category: category.value,
-    };
-    console.log(products);
+        id: Date.now(),
+        name: names.value,
+        description: description.value,
+        price: price.value,
+        img_url: img_url.value,
+        category: category.value,
+        quantity: 0
+    }
+    console.log(products)
     staticProducts.push(products);
-    window.localStorage.setItem(
-      "ArrayOfProducts",
-      JSON.stringify(staticProducts)
-    );
-    names.value = "";
-    description.value = "";
-    price.value = "";
-    img_url.value = "";
-    category.value = "";
-    sessionStorage.setItem("productID", null);
-  });
-
-  function cancelOperation() {
-    sessionStorage.setItem("productID", null);
-  }
-}
+    window.localStorage.setItem('ArrayOfProducts', JSON.stringify(staticProducts));
+    names.value=""
+    description.value=""
+    price.value=""
+    img_url.value=""
+    category.value=""
+});
