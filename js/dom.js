91edfdf2ea5d3025d5a9551bdb058cec62f6ae34
Rabee <ralbhaisi96@gmail.com>
2022-07-27 09:41:09 +0300

    Add these files index.js cart.js home.js form.js related #18

---
+++ js/dom.js
@@ -0,0 +1,4 @@
+const btnGoAsAdmin = document.getElementById("btnAdmin")
+const btnGoAsCustomer = document.getElementById("btnBuyer")
+const btnSignOut = document.getElementById("btnSignOut")
+
---
+++ js/index.js
@@ -0,0 +1,12 @@
+btnGoAsAdmin.addEventListener("click", signInAsAdmin);
+btnGoAsCustomer.addEventListener("click", signInAsCustomer);
+
+function signInAsAdmin() {
+  localStorage.setItem("isAdmin", true);
+  location.replace("./home-page.html");
+}
+
+function signInAsCustomer() {
+  localStorage.setItem("isAdmin", false);
+  location.replace("./home-page.html");
+}
